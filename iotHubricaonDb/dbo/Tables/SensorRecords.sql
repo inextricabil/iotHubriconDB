fc0663780bb0e83eaa8767921f6c201f7bae9ac8
Serban Boghiu <pincellodev14@outlook.com>
2017-11-16 13:40:33 +0200

    Initial commit

---
+++ iotHubricaonDb/dbo/Tables/SensorRecords.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[SensorRecords] (
+    [SensorId]    UNIQUEIDENTIFIER NOT NULL,
+    [Temperature] REAL             NOT NULL,
+    [Humidity]    REAL             NOT NULL,
+    CONSTRAINT [PK_dbo.SensorRecords] PRIMARY KEY CLUSTERED ([SensorId] ASC)
+);
+
